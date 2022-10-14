1fec6d8d1310220c0de5d5ddf3f1f769311d754d
Mikhail <miwanya.spb@gmail.com>
2022-10-14 03:35:42 +0300

    add Examples003RandomSum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine("первое рандомное число" + " " + numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine("второе рандомное число" + " " + numberB);
 int resul = numberA + numberB;
-Console.WriteLine(resul);
+Console.WriteLine("Сумма " + " = " + resul);
